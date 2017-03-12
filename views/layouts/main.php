77cf13d7e673b38e1b1854d2afeecb737415c435
kontroller <volt_mk@ukr.net>
2017-03-12 09:32:43 +0200

    commit

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -147,7 +147,7 @@ AppAsset::register($this);
             </div><!--/header-bottom-->
         </header><!--/header-->
 
-        <?= $content ?>
+        <?= $content; ?>
 
         <footer id="footer"><!--Footer-->
             <div class="footer-top">
